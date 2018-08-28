8e31e599bbcc106056ddddf752858d590f078d80
Talco88 <talcohen88@gmail.com>
2018-08-28 18:37:05 +0300

    Add first server interface

---
+++ TemplateCore/TemplateCoreBusiness/Engine/IAppEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemplateCoreBusiness.Models;
+
+namespace TemplateCoreBusiness.Engine
+{
+    interface IAppEngine
+    {
+        string GenerateTemplate();
+        string GetTemplate(string iTemplateName);
+        List<string> GetTemplateFromSearch(string iSearchKey);
+        void CreateNewTemplate(GenerateNewTemplateEntity iNewTemplateData);
+    }
+}
