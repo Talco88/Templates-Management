533221a0681cceacf7bf29216c13fb5ce4623f3d
Or Horovitz <orho@mail.mta.ac.il>
2018-10-29 21:43:04 +0200

    Added option to save template

--- TemplateCore/TemplateCore/WebApp/JS/createTemplatePage.js
+++ TemplateCore/TemplateCore/WebApp/JS/createTemplatePage.js
@@ -19,7 +19,31 @@ createTemplatesPage.onPagedRecived = function () {
     setTimeout(function () {
         $("#MainAppWindow").html(Global_Creat_Template_BaseHTMLData);
         $(".dynamic-category-name").html(createTemplatesPage.CategoryName);
+
+        let saveTemplatetnBtn = document.querySelector("#registerTemplate-btn");
+        saveTemplatetnBtn.onclick = createTemplatesPage.onSaveTemplatetnBtnTemplatetnBtn;
     }, 1);
+}
+
+createTemplatesPage.onSaveTemplatetnBtnTemplatetnBtn = function () {
+    let templateContent = document.getElementById("message").value;
+    let templateName = document.getElementById("templateName").value;
+    if (templateName && templateName !== "") {
+        let newTemplateContent = "{\"Template\": \"<p>" + templateContent + "</p>\", \"numberOfChanges\": 0}";
+        Platform.CreateNewTemplate(newTemplateContent.toString(), templateName, createTemplatesPage.CategoryName, true, createTemplatesPage.onSaveTemplatetnBtnTemplatetnBtnResponce);
+    }
+    else {
+        alert("You must enter name to the new template");
+    }
+}
+
+createTemplatesPage.onSaveTemplatetnBtnTemplatetnBtnResponce = function (iServerResponce) {
+    if (iServerResponce.StatusCode === 0) {
+        templatesPage.setPage();
+    }
+    else {
+        alert("Failed to save the new Template\n" + iServerResponce.RetObject);
+    }
 }
 
 createTemplatesPage.LoadLobbyPageRes = function (isSet) {
