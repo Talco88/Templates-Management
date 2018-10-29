7ca0d9ab8e56065767b3e34b20fc98b0cd09e211
Or Horovitz <orho@mail.mta.ac.il>
2018-10-29 22:41:12 +0200

    Added placeHolder to createTemplate

--- TemplateCore/TemplateCore/WebApp/JS/createTemplatePage.js
+++ TemplateCore/TemplateCore/WebApp/JS/createTemplatePage.js
@@ -21,23 +21,36 @@ createTemplatesPage.onPagedRecived = function () {
         $(".dynamic-category-name").html(createTemplatesPage.CategoryName);
 
         let saveTemplatetnBtn = document.querySelector("#registerTemplate-btn");
-        saveTemplatetnBtn.onclick = createTemplatesPage.onSaveTemplatetnBtnTemplatetnBtn;
+        saveTemplatetnBtn.onclick = createTemplatesPage.onSaveTemplatetnBtn;
+
+        let placeHolderBtn = document.querySelector("#InsertPlaceHolder-btn");
+        placeHolderBtn.onclick = createTemplatesPage.onPlaceHolderBtnTemplate;
     }, 1);
 }
 
-createTemplatesPage.onSaveTemplatetnBtnTemplatetnBtn = function () {
+createTemplatesPage.onPlaceHolderBtnTemplate = function () {
+    let templateContent = document.getElementById("message");
+    let templateContentValue = templateContent.value;
+    let placeHolderNameValue = document.getElementById("placeHolderName").value;
+    templateContentValue = templateContentValue + " $" + placeHolderNameValue + " ";
+    templateContent.value = templateContentValue;
+}
+
+createTemplatesPage.onSaveTemplatetnBtn = function () {
     let templateContent = document.getElementById("message").value;
+    let templateContentAfterReplace = templateContent.split("\n").join("</br>");
+    console.log("templateContentAfterReplace: ", templateContentAfterReplace);
     let templateName = document.getElementById("templateName").value;
     if (templateName && templateName !== "") {
-        let newTemplateContent = "{\"Template\": \"<p>" + templateContent + "</p>\", \"numberOfChanges\": 0}";
-        Platform.CreateNewTemplate(newTemplateContent.toString(), templateName, createTemplatesPage.CategoryName, true, createTemplatesPage.onSaveTemplatetnBtnTemplatetnBtnResponce);
+        let newTemplateContent = "{\"Template\": \"<p>" + templateContentAfterReplace + "</p>\", \"numberOfChanges\": 0}";
+        Platform.CreateNewTemplate(newTemplateContent.toString(), templateName, createTemplatesPage.CategoryName, true, createTemplatesPage.onSaveTemplatetnBtnResponce);
     }
     else {
         alert("You must enter name to the new template");
     }
 }
 
-createTemplatesPage.onSaveTemplatetnBtnTemplatetnBtnResponce = function (iServerResponce) {
+createTemplatesPage.onSaveTemplatetnBtnResponce = function (iServerResponce) {
     if (iServerResponce.StatusCode === 0) {
         templatesPage.setPage();
     }
